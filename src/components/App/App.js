7d6253ca7b305c74441cef64b3cb93ac149a517f
er-nick-cr <nikiteremeev2010@gmail.com>
2021-08-04 23:06:54 +0700

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+import { Route, Switch, useHistory } from 'react-router-dom';
+import Header from '../Header/Header';
+import Main from '../Main/Main';
+import Footer from '../Footer/Footer';
+
+function App() {
+	return (
+		<div className="page">
+			<Header />
+			<Main />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
